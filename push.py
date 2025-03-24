7dd315b90f163e82cf5dc1f2275d737343c07d5d
cmdSTARMO <61082716+cmdSTARMO@users.noreply.github.com>
2025-03-24 14:17:12 +0800

    Added Checkablility

--- push.py
+++ push.py
@@ -172,6 +172,18 @@ def log_push_event_csv(subject, receivers, error_message=None):
         writer.writerow(["时间（北京时间）", "接收人", "标题", "错误状态"])
         writer.writerows(new_rows + existing_rows)
 
+def load_receivers_from_file(filepath="email_list.txt"):
+    with open(filepath, 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    # 替换换行为逗号，再按逗号分割
+    raw_emails = content.replace('\n', ',').split(',')
+
+    # 去除空格、空项
+    cleaned_emails = [email.strip() for email in raw_emails if email.strip()]
+    return cleaned_emails
+
+
 
 if __name__ == "__main__":
     # 用户输入
@@ -445,7 +457,10 @@ if __name__ == "__main__":
     subject = generate_subject()
 
     message = create_email_content(mail_msg, subject, sender_nickname, sender_email, receiver_nickname, receiver_email)
-    receivers = ['1624070280@qq.com', 'heli2002@163.com', 'xin.jackhuang@gmail.com', 'huangdapao@huangdapao.com' , '2248362474@qq.com', '344621206@qq.com', '484420009@qq.com', '980364480@qq.com', '1097442370@qq.com', 'solid_b1n@qq.com', "944240869@qq.com", '2366965809@qq.com', '675677537@qq.com']  # 可以包含多个接收者邮箱地址 'solid_b1n@qq.com', "yfchan484420009@163.com", "944240869@qq.com"
+    # receivers = ['1624070280@qq.com', 'heli2002@163.com', 'xin.jackhuang@gmail.com', 'huangdapao@huangdapao.com' , '2248362474@qq.com', '344621206@qq.com', '484420009@qq.com', '980364480@qq.com', '1097442370@qq.com', 'solid_b1n@qq.com', "944240869@qq.com", '2366965809@qq.com', '675677537@qq.com']  # 可以包含多个接收者邮箱地址 'solid_b1n@qq.com', "yfchan484420009@163.com", "944240869@qq.com"
+    # 加载收件人邮箱
+    # receivers = ['1624070280@qq.com']
+    receivers = load_receivers_from_file("email_list.txt")
     bot = FeishuBot("https://open.feishu.cn/open-apis/bot/v2/hook/f129a3a4-9860-4917-9b14-5e63f9bf8e98")
 
     try:
